cd9dd92d4b733ae043685701a5af501d9809c8c6
Simon Dowdeswell <simon.dowdeswell@rockend.com.au>
2020-08-16 11:37:38 +1000

    add swagger generation

--- api/bushApp.api/Startup.cs
+++ api/bushApp.api/Startup.cs
@@ -26,11 +26,18 @@ namespace bushApp.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{this.GetType().Namespace} API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
